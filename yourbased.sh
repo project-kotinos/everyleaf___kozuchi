9814e1c500d865c6220ae6924e8442038a34fb20
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-07 22:54:25 -0400

    Create yourbased.sh

---
+++ yourbased.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ex
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install tzdata -y
+gem install bundler -v 2.0.1
+apt get install fonts-ipafont
+
+
+
+
+#install
+bundle install --jobs=3 --retry=3
+
+# before_script
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+
+#script
+rake
