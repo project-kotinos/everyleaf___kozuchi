aae8cb524f2157f36fe2cc8fe4f98d45a00b46e4
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-12 21:24:56 +0000

    add phantomjs

--- yourbased.sh
+++ yourbased.sh
@@ -2,9 +2,15 @@
 set -ex
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y tzdata fonts-ipafont libpq-dev
+apt-get update && apt-get install -y tzdata fonts-ipafont libpq-dev libyaml-dev postgresql-client libfontconfig
 gem install bundler -v 2.0.1
- 
+export DATABASE_CLEANER_ALLOW_REMOTE_DATABASE_URL=true 
+
+rm -rf $PWD/travis_phantomjs; mkdir -p $PWD/travis_phantomjs
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar -xvf $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C $PWD/travis_phantomjs
+PATH=$PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64/bin:$PATH
+phantomjs --version
 
 
 #install
